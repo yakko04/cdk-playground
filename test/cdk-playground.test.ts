369660f6d018537b4628c2bdf4436a6390fcb429
yakko04 <yakyukozo2001@gmail.com>
2026-03-24 19:59:07 +0900

    Initial commit

---
+++ test/cdk-playground.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPlayground from '../lib/cdk-playground-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-playground-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPlayground.CdkPlaygroundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
